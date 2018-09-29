d8708049d8503e26b795b5dfa80ddc91ec9965ef
bfzheng <baofeng_zheng@163.com>
2018-09-29 22:46:30 +0800

    add User model

---
+++ src/main/java/com/thoughtworks/training/userservice/model/User.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.training.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String password;
+}
